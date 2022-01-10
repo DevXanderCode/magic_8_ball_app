e97710a6178ef98609fbb402df922f966ee6b290
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-10 22:05:17 +0100

    added the text button to the ball image

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+        ),
+        backgroundColor: Colors.blueAccent.shade700,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          print("Ball pressed");
+        },
+        child: Container(
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
